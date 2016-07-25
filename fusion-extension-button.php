1deaae5f80881e17ff52ea30dc28f918af3018da
Jeff Chew <jeff.chew@agencydominion.com>
2016-07-25 12:23:53 -0300

    Version 1.0.0

---
+++ fusion-extension-button.php
@@ -0,0 +1,64 @@
+<?php
+/**
+ * @package Fusion_Extension_Button
+ */
+ 
+/**
+ * Plugin Name: Fusion : Extension - Button
+ * Plugin URI: http://fusion.1867dev.com
+ * Description: Button Extension Package for Fusion.
+ * Version: 1.0.0
+ * Author: Agency Dominion
+ * Author URI: http://agencydominion.com
+ * License: GPL2
+ */
+ 
+/**
+ * FusionExtensionButton class.
+ *
+ * Class for initializing an instance of the Fusion Button Extension.
+ *
+ * @since 1.0.0
+ */
+
+class FusionExtensionButton	{ 
+	public function __construct() {
+						
+		// Initialize the language files
+		load_plugin_textdomain( 'fusion-extension-button', false, plugin_dir_url( __FILE__ ) . 'languages' );
+		
+		// Enqueue admin scripts and styles
+		add_action('admin_enqueue_scripts', array($this, 'admin_enqueue_scripts_styles'));
+		
+	}
+	
+	/**
+	 * Enqueue JavaScript and CSS on Admin pages.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @param string $hook_suffix The current admin page.
+	 */
+	 
+	public function admin_enqueue_scripts_styles($hook_suffix) {
+		global $post;
+		
+		$options = get_option('fsn_options');
+		$fsn_post_types = !empty($options['fsn_post_types']) ? $options['fsn_post_types'] : '';
+		
+		// Editor scripts and styles
+		if ( ($hook_suffix == 'post.php' || $hook_suffix == 'post-new.php') && (!empty($fsn_post_types) && is_array($fsn_post_types) && in_array($post->post_type, $fsn_post_types)) ) {
+			wp_enqueue_script( 'fsn_button_admin', plugin_dir_url( __FILE__ ) . 'includes/js/fusion-extension-button-admin.js', array('jquery'), '1.0.0', true );
+		}
+	}
+
+}
+
+$fsn_extension_button = new FusionExtensionButton();
+
+//EXTENSIONS
+
+//Button
+require_once('includes/extensions/button.php');
+
+?>
---
+++ includes/extensions/button.php
@@ -0,0 +1,284 @@
+<?php
+/**
+ * @package Fusion_Extension_Button
+ */
+
+/**
+ * Button Extension
+ *
+ * Class for adding a Button element to the Fusion engine
+ *
+ * @since 1.0.0
+ */
+ 
+class FusionButton	{
+
+	public function __construct() {
+		
+		//add button shortcode
+		add_shortcode('fsn_button', array($this, 'button_shortcode'));
+		
+		//load button layout via AJAX
+		add_action('wp_ajax_button_load_layout', array($this, 'load_button_layout'));
+		
+		//load saved button layout fields
+		add_filter('fsn_element_params', array($this, 'load_saved_button_layout_fields'), 10, 3);
+		
+		//initialize button
+		add_action('init', array($this, 'init_button'), 12);
+				
+		//add standard button layout
+		add_filter('add_button_layout', array($this, 'standard_layout'));
+		
+	}
+	
+	/**
+	 * Load Button Layout
+	 *
+	 * @since 1.0.0
+	 */
+	 
+	public function load_button_layout() {
+		global $fsn_button_layouts;
+		$button_layout = $_POST['button_layout'];
+		
+		if (!empty($fsn_button_layouts) && !empty($button_layout)) {
+			$response_array = array();
+			foreach($fsn_button_layouts[$button_layout]['params'] as $param) {						
+				$param_value = '';
+				$param['section'] = !empty($param['section']) ? $param['section'] : 'general';
+				//check for dependency
+				$dependency = !empty($param['dependency']) ? true : false;
+				if ($dependency === true) {
+					$depends_on_field = $param['dependency']['param_name'];
+					$depends_on_not_empty = !empty($param['dependency']['not_empty']) ? $param['dependency']['not_empty'] : false;
+					if (!empty($param['dependency']['value']) && is_array($param['dependency']['value'])) {
+						$depends_on_value = esc_attr(json_encode($param['dependency']['value']));
+					} else if (!empty($param['dependency']['value'])) {
+						$depends_on_value = $param['dependency']['value'];
+					} else {
+						$depends_on_value = '';
+					}
+					
+					$dependency_callback = !empty($param['dependency']['callback']) ? $param['dependency']['callback'] : '';
+					$dependency_string = ' data-dependency-param="'. $depends_on_field .'"'. ($depends_on_not_empty === true ? ' data-dependency-not-empty="true"' : '') . (!empty($depends_on_value) ? ' data-dependency-value="'. $depends_on_value .'"' : '') . (!empty($dependency_callback) ? ' data-dependency-callback="'. $dependency_callback .'"' : '');
+				}
+				$param_output = '<div class="form-group button-layout'. ( !empty($param['class']) ? ' '. $param['class'] : '' ) .'"'. ( $dependency === true ? $dependency_string : '' ) .'>';
+					$param_output .= FusionCore::get_input_field($param, $param_value);
+				$param_output .= '</div>';
+				$response_array[] = array(
+					'section' => $param['section'],
+					'output' => $param_output
+				);
+			}
+		}
+		
+		header('Content-type: application/json');
+		
+		echo json_encode($response_array);
+		
+		exit;
+	}
+	
+	/**
+	 * Load Saved Button Layout Fields
+	 *
+	 * @since 1.0.0
+	 */
+	 
+	public function load_saved_button_layout_fields($params, $shortcode, $saved_values) {
+	
+		global $fsn_button_layouts;
+		
+		//load standard layout by default
+		$saved_values['button-layout'] = !empty($saved_values['button-layout']) ? $saved_values['button-layout'] : 'standard';
+		
+		if ($shortcode == 'fsn_button' && !empty($saved_values['button-layout']) && array_key_exists($saved_values['button-layout'], $fsn_button_layouts)) {
+			$saved_layout = $saved_values['button-layout'];
+			$params_to_add = $fsn_button_layouts[$saved_layout]['params'];
+			for ($i=0; $i < count($params_to_add); $i++) {
+				if (empty($params_to_add[$i]['class'])) {
+					$params_to_add[$i]['class'] = 'button-layout';
+				} else {
+					$params_to_add[$i]['class'] .= ' button-layout';
+				}
+			}
+			//add layout params to initial load
+			array_splice($params, 1, 0, $params_to_add);
+		}
+		
+		return $params;
+	}
+	
+	/**
+	 * Initialize Button
+	 *
+	 * @since 1.0.0
+	 */
+	 
+	public function init_button() {
+	
+		//MAP SHORTCODE
+		if (function_exists('fsn_map')) {						
+			
+			//define button layouts
+			$button_layouts = array();
+			
+			//get layouts
+			$button_layouts = apply_filters('add_button_layout', $button_layouts);
+			
+			//create button layouts global
+			global $fsn_button_layouts;
+			$fsn_button_layouts = $button_layouts;
+			
+			//pass layouts array to script
+			wp_localize_script('fsn_button_admin', 'fsnButton', $button_layouts);
+			
+			//get button layout options
+			if (!empty($button_layouts)) {
+				$button_layout_options = array();
+				$smart_supported = array();
+				$layout_specific_params = array();
+				foreach($button_layouts as $key => $value) {
+					//create array of layouts for select layout dropdown
+					$button_layout_options[$key] = $value['name'];
+				}
+				//add layout list items to global
+				foreach($button_layouts as $button_layout) {
+					foreach($button_layout['params'] as $button_layout_param) {
+						if ($button_layout_param['type'] == 'custom_list') {
+							global $fsn_custom_lists;	
+							$fsn_custom_lists[$button_layout_param['id']]['parent'] = 'fsn_button';
+							$fsn_custom_lists[$button_layout_param['id']]['params'] = $button_layout_param['item_params'];
+						}
+					}
+				}
+			}
+									
+			$params_array = array(		
+				array(
+					'type' => 'select',
+					'options' => $button_layout_options,
+					'param_name' => 'button_layout',
+					'label' => __('Type', 'fusion-extension-button')
+				)		
+			);
+			
+			fsn_map(array(
+				'name' => __('Button', 'fusion-extension-button'),
+				'shortcode_tag' => 'fsn_button',
+				'description' => __('Add button. Buttons can link to external or internal content.', 'fusion-extension-button'),
+				'icon' => 'add_box',
+				'disable_style_params' => array('text_align','text_align_xs','font_size','color'),
+				'params' => $params_array
+			));
+		}
+	}
+	
+	/**
+	 * Button shortcode
+	 *
+	 * @since 1.0.0
+	 *
+	 * @param array $atts The shortcode attributes.
+	 * @param string $content The shortcode content.
+	 */
+	
+	public function button_shortcode( $atts, $content ) {		
+		extract( shortcode_atts( array(			
+			'button_layout' => 'standard'
+		), $atts ) );
+		
+		$output = '';
+		
+		if (!empty($button_layout)) {
+			$callback_function = 'fsn_get_'. $button_layout .'_button';
+			$output .= '<div class="fsn-button '. $button_layout .' '. fsn_style_params_class($atts) .'">';
+				$output .= call_user_func($callback_function, $atts, $content);
+			$output .= '</div>';
+		}
+		
+		return $output;
+	}
+	
+	/**
+	 * Standard Button layout
+	 */
+	 
+	public function standard_layout($button_layouts) {
+		
+		$button_style_options = array(
+			'' => __('Default', 'fusion-extension-button'),
+			'btn btn-default' => __('Button - Default', 'fusion-extension-button'),
+			'btn btn-primary' => __('Button - Primary', 'fusion-extension-button'),
+			'btn btn-success' => __('Button - Success', 'fusion-extension-button'),
+			'btn btn-info' => __('Button - Info', 'fusion-extension-button'),
+			'btn btn-warning' => __('Button - Warning', 'fusion-extension-button'),
+			'btn btn-danger' => __('Button - Danger', 'fusion-extension-button'),
+			'btn btn-link' => __('Button - Link', 'fusion-extension-button')	
+		);
+		$button_style_options = apply_filters('fsn_button_style_options', $button_style_options);
+		
+		//standard button layout
+		$standard_layout = array(
+			'name' => __('Standard', 'fusion-extension-button'),
+			'params' => array(
+				array(
+					'type' => 'button',
+					'param_name' => 'button',
+					'label' => __('Button', 'fusion-extension-button'),
+					'help' => __('Link to external or internal content.', 'fusion-extension-button')
+				),
+				array(
+					'type' => 'select',
+					'options' => $button_style_options,
+					'param_name' => 'button_style',
+					'label' => __('Style', 'fusion-extension-button')
+				),
+				array(
+					'type' => 'checkbox',
+					'param_name' => 'button_btn_block',
+					'label' => __('Full Width Button', 'fusion-extension-button'),
+					'help' => __('Make button span the full width of the Column.' , 'fusion-extension-button'),
+					'dependency' => array(
+						'param_name' => 'button_style',
+						'value' => array('btn btn-default', 'btn btn-primary', 'btn btn-success', 'btn btn-info', 'btn btn-warning', 'btn btn-danger', 'btn btn-link')
+					)
+				)
+			)
+		);
+		$button_layouts['standard'] = $standard_layout;
+		
+		return $button_layouts;
+	}
+ 
+}
+ 
+$fsn_button = new FusionButton();
+
+//STANDARD Button
+
+//render button layout ** function name must follow fsn_get_[button layout key]_button
+function fsn_get_standard_button($atts = false, $content = false) {
+	extract( shortcode_atts( array(
+		'button' => '',
+		'button_style' => '',
+		'button_btn_block' => ''
+	), $atts ) );
+	
+	$output = '';
+	
+	if (!empty($button)) {
+		$button_object = fsn_get_button_object($button);
+		$button_classes = !empty($button_style) ? $button_style : '';
+		if (!empty($button_classes)) {
+			$button_classes .= !empty($button_btn_block) ? ' btn-block' : '';
+		}
+		
+		$output .= apply_filters('fsn_standard_button_output', '<a'. fsn_get_button_anchor_attributes($button_object, $button_classes) .'>'. $button_object['button_label'] .'</a>', $atts);
+	}
+	
+	return $output;
+}
+
+?>

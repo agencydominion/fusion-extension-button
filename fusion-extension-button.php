06cb583a1c25a50dff25329ecef51906ea448a61
Jeff Chew <jeff.chew@agencydominion.com>
2018-09-27 10:21:23 -0300

    Update version number

--- fusion-extension-button.php
+++ fusion-extension-button.php
@@ -2,19 +2,19 @@
 /**
  * @package Fusion_Extension_Button
  */
- 
+
 /**
  * Plugin Name: Fusion : Extension - Button
  * Plugin URI: http://www.agencydominion.com/fusion/
  * Description: Button Extension Package for Fusion.
- * Version: 1.1.6
+ * Version: 1.1.7
  * Author: Agency Dominion
  * Author URI: http://agencydominion.com
  * Text Domain: fusion-extension-button
  * Domain Path: /languages/
  * License: GPL2
  */
- 
+
 /**
  * FusionExtensionButton class.
  *
@@ -23,28 +23,28 @@
  * @since 1.0.0
  */
 
-class FusionExtensionButton	{ 
+class FusionExtensionButton	{
 	public function __construct() {
-		
+
 		// Initialize the language files
 		add_action('plugins_loaded', array($this, 'load_textdomain'));
-						
+
 		// Enqueue admin scripts and styles
 		add_action('admin_enqueue_scripts', array($this, 'admin_enqueue_scripts_styles'));
-		
+
 	}
-	
+
 	/**
 	 * Load Textdomain
 	 *
 	 * @since 1.1.6
 	 *
 	 */
-	 
+
 	public function load_textdomain() {
 		load_plugin_textdomain( 'fusion-extension-button', FALSE, basename( dirname( __FILE__ ) ) . '/languages/' );
 	}
-	
+
 	/**
 	 * Enqueue JavaScript and CSS on Admin pages.
 	 *
@@ -52,13 +52,13 @@ class FusionExtensionButton	{
 	 *
 	 * @param string $hook_suffix The current admin page.
 	 */
-	 
+
 	public function admin_enqueue_scripts_styles($hook_suffix) {
 		global $post;
-		
+
 		$options = get_option('fsn_options');
 		$fsn_post_types = !empty($options['fsn_post_types']) ? $options['fsn_post_types'] : '';
-		
+
 		// Editor scripts and styles
 		if ( ($hook_suffix == 'post.php' || $hook_suffix == 'post-new.php') && (!empty($fsn_post_types) && is_array($fsn_post_types) && in_array($post->post_type, $fsn_post_types)) ) {
 			wp_enqueue_script( 'fsn_button_admin', plugin_dir_url( __FILE__ ) . 'includes/js/fusion-extension-button-admin.js', array('jquery'), '1.0.0', true );
